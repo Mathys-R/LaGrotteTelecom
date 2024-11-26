27742c8a0b83116b9042c339d12ede214bc332be
MR- <Mathys.rosinski@gmail.com>
2024-11-26 12:03:29 +0100

    Modifying Characters trait

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Game grotteImt = new Game();
+        System.out.println("Java project by Louison Prodhomme and Mathys Rosinski for IMT Nord Europe");
     }
 }
